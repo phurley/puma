f91308720ceaabdaef8cc456360077aaeff74705
Patrick Hurley <phurley@gmail.com>
2013-11-07 08:48:02 -0500

    Fix windows devkit build, will need conditional for windows

--- ext/puma_http11/extconf.rb
+++ ext/puma_http11/extconf.rb
@@ -3,6 +3,6 @@ require 'mkmf'
 dir_config("puma_http11")
 
 $defs.push "-Wno-deprecated-declarations"
-$libs += " -lssl -lcrypto "
+$libs += " -lssl -lcrypto -gdi32 "
 
 create_makefile("puma/puma_http11")
