bb40068d0034d43b8202ce64838bd883df4f86cf
Patrick Hurley <phurley@gmail.com>
2013-11-07 08:55:37 -0500

    Fix missing -l flag

--- ext/puma_http11/extconf.rb
+++ ext/puma_http11/extconf.rb
@@ -3,6 +3,6 @@ require 'mkmf'
 dir_config("puma_http11")
 
 $defs.push "-Wno-deprecated-declarations"
-$libs += " -lssl -lcrypto -gdi32 "
+$libs += " -lssl -lcrypto -lgdi32 "
 
 create_makefile("puma/puma_http11")
